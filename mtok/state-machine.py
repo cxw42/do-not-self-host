6d9303cc42997021bed0912843005846465a8695
Chris White <cxwembedded@gmail.com>
2020-06-03 21:19:01 -0400

    Fix shebang [minor]

--- mtok/state-machine.py
+++ mtok/state-machine.py
@@ -1,7 +1,7 @@
-#!python3
+#!/usr/bin/env python3
 # state-machine.py: Convert a state machine from
 # https://cyberzhg.github.io/toolbox/nfa2dfa, saved as a CSV, into ngb asm.
-# Copyright (c) 2018 cxw42.  Licensed MIT.
+# Copyright (c) 2018, 2020 cxw42.  Licensed MIT.
 #
 # Workflow (Google Chrome):
 #   - Create the regex and state machine in nfa2dfa
